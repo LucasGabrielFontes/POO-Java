7294f7bcf8d001fbcedaf7aee7bb1741eab72a42
Lucas Gabriel Fontes da Silva <lucasgabrielfontesdasilva@gmail.com>
2024-03-22 09:47:26 -0300

    Update Main.java

--- BeeCrowd/GoodDemais/1169-TrigoNoTabuleiro/Main.java
+++ BeeCrowd/GoodDemais/1169-TrigoNoTabuleiro/Main.java
@@ -1,5 +1,10 @@
+// Autor: Lucas Gabriel Fontes da Silva
+// Descrição: Este programa calcula o peso total de grãos de trigo em quilogramas, dado o número de quadrados de um tabuleiro de xadrez: o primeiro possui um grão e a quantidade do próximo é o dobro do anterior. O peso 12 grãos é de 0,001kg.
+
 import java.util.Scanner;
 import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
 
 public class Main {
 
@@ -7,22 +12,24 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
 
-        int testes = input.nextInt();
+        long testes = input.nextInt();
 
-        for (int i = 0; i < testes; i++) {
+        for (long i = 0; i < testes; i++) {
 
-            long totGraos = 1;
-            long graos = 1;
-            int quadrados = input.nextInt();
+            BigInteger totGraos = BigInteger.ONE; // .ONE é um método estático da classe BigInteger que retorna o valor 1
+            BigInteger graos = BigInteger.ONE;
+            long quadrados = input.nextInt();
 
-            for (int aux = 1; aux < quadrados; aux++) {
-                graos *= 2;
-                totGraos += graos;
+            for (long aux = 1; aux < quadrados; aux++) {
+                graos = graos.multiply(BigInteger.valueOf(2));
+                totGraos = totGraos.add(graos);
             }
 
-            double pesoTot = (totGraos*0.001)/12;
+            BigDecimal pesoTot = new BigDecimal(totGraos)
+                    .multiply(BigDecimal.valueOf(0.001))
+                    .divide(BigDecimal.valueOf(12), 2, RoundingMode.HALF_DOWN); // Arredondamento para baixo com duas casas decimais
 
-            System.out.printf("%.0f kg\n", Math.floor(pesoTot));
+            System.out.printf("%.0f kg\n", pesoTot.setScale(0, RoundingMode.DOWN));
 
         }
 
